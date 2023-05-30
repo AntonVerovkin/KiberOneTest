00d3ebefc898564a2b4e8a9f0ba1e5be97911282
AntonVerovkin <anton.veryovkin@gmail.com>
2023-05-30 14:21:15 +0200

    DZ Ne proveral, t.k. ostalas problema s proschlogo saniatia

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -10,13 +10,12 @@ public class TestRunner {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.getDriver();
         driver.get(ConfigProvider.getBASE_URL());
-        WebElement elTitle = driver.findElement(By.xpath("//div[@class='logo']//span[contains(text(), 'Все цены')]"
-                ));
+        WebElement elTitle = driver.findElement(By.xpath("//a[@href='/ct/2133/']"));
 
-        WebElement elHeadPhonesCta = driver.findElement(By.xpath("//div[@class='base']//a[@href='/ct/1004/]"));
+        //WebElement elHeadPhonesCta = driver.findElement(By.xpath("//div[@class='base']//a[@href='/ct/1004/]"));
         WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(ExpectedConditions.elementToBeClickable(elTitle));
-        elHeadPhonesCta.click();
+        elTitle.click();
         driver.quit();
     }
 }
